862fb5d0949b585c3421a1d1a9c49dbba96ad2f5
nux <nux@example.com>
2020-08-23 21:48:16 -0700

    more adjustments to advanced pause for TITAN

--- Marlin/SKR-EZABL-TITAN-Nuxs-Personal-Configuration_adv.h
+++ Marlin/SKR-EZABL-TITAN-Nuxs-Personal-Configuration_adv.h
@@ -2040,7 +2040,7 @@
                                                   // This short retract is done immediately, before parking the nozzle.
   #define FILAMENT_CHANGE_UNLOAD_FEEDRATE      6  // (mm/s) Unload filament feedrate. This can be pretty fast.    //  changed by Nux   was 10
   #define FILAMENT_CHANGE_UNLOAD_ACCEL        25  // (mm/s^2) Lower acceleration may allow a faster feedrate.
-  #define FILAMENT_CHANGE_UNLOAD_LENGTH       45  // (mm) The length of filament for a complete unload.     //  changed by Nux   was 100
+  #define FILAMENT_CHANGE_UNLOAD_LENGTH       55  // (mm) The length of filament for a complete unload.     //  changed by Nux   was 100
                                                   //   For Bowden, the full length of the tube and nozzle.
                                                   //   For direct drive, the full length of the nozzle.
                                                   //   Set to 0 for manual unloading.
@@ -2049,7 +2049,7 @@
                                                   // 0 to disable start loading and skip to fast load only
   #define FILAMENT_CHANGE_FAST_LOAD_FEEDRATE   6  // (mm/s) Load filament feedrate. This can be pretty fast.
   #define FILAMENT_CHANGE_FAST_LOAD_ACCEL     25  // (mm/s^2) Lower acceleration may allow a faster feedrate.
-  #define FILAMENT_CHANGE_FAST_LOAD_LENGTH     5  // (mm) Load length of filament, from extruder gear to nozzle. //  changed by Nux   was 0
+  #define FILAMENT_CHANGE_FAST_LOAD_LENGTH    20  // (mm) Load length of filament, from extruder gear to nozzle. //  changed by Nux   was 0
                                                   //   For Bowden, the full length of the tube and nozzle.
                                                   //   For direct drive, the full length of the nozzle.
   #define ADVANCED_PAUSE_CONTINUOUS_PURGE         // Purge continuously up to the purge length until interrupted.  //  uncommented by Nux
